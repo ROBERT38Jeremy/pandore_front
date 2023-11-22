e437c93c3a34d320648aac3e6e8b18cbde80da2e
Jérémy <jeremy100401@gmail.com>
2023-11-22 14:08:13 +0100

    display databases list in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,29 @@
 <script setup>
+import { onBeforeMount, ref, watchEffect } from 'vue';
+import { useAxios } from '../hooks/useAxios';
+import { useDBConnectStore } from '../stores/DBConnect'
+import { storeToRefs } from 'pinia';
+import CustomLoader from '../components/global/CustomLoader.vue'
+
+const { setDatabase } = useDBConnectStore()
+const { database } = storeToRefs(useDBConnectStore())
+const databaseList = ref({});
+const loading = ref(false);
+
+const getDatabaseList = () => {
+    const result = ref({});
+    loading.value = true;
+    result.value = useAxios({ url: `/database/list`, method: 'GET' });
+
+    watchEffect(() => {
+        if (result.value.isLoading === false && result.value.resp.data.success) {
+            databaseList.value = result.value.resp.data.success;
+            loading.value = false;
+        }
+    })
+}
+
+onBeforeMount(getDatabaseList);
 </script>
 
 <template>
@@ -10,6 +35,11 @@
             <span>RE</span>
         </div>
         <p>Select a Database First.</p>
+        <CustomLoader :loading="loading">
+            <div class="databases-list">
+                <RouterLink v-for="database in databaseList" :to="'/database/'+database+'/structure'">{{ database }}</RouterLink>
+            </div>
+        </CustomLoader>
     </div>
 </template>
 
@@ -18,16 +48,34 @@
     user-select: none;
 }
 div.container {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     text-align: center;
+    width: 100%;
+    height: 100%;
 }
 div.flex-container {
     display: flex;
+    justify-content: center;
     align-items: center;
 }
+
+div.databases-list {
+    display: flex;
+    gap: 1em 3em;
+    align-items: stretch;
+    padding: 0 3em;
+}
+
+div.databases-list>a {
+   padding: 2em 1em;
+   border: 1px solid var(--color-border);
+   border-radius: 0.2em;
+   cursor: pointer;
+   transition: all 0.1s ease-in-out;
+}
+
+div.databases-list>a:hover {
+    background-color: var(--color-background);
+ }
 
 p {
     font-style: italic;
